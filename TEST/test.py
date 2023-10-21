00944af68965e37533a14cf7c608a13bbb8aee42
김대희 <asrf13@naver.com>
2023-10-21 10:22:34 +0900

    간단자료형

--- TEST/test.py
+++ TEST/test.py
@@ -2,12 +2,23 @@
 
 print(탈모탄조[0:3])
 
-중고차 = ['Polestar 2','Snow',5700]
+
+
+중고차 = [ 'Polestar 2','Snow',5700 ] # List
 중고차[2] = 4600
+print(중고차)
+
+중고차2 = { 
+    'Brand' : 'HYUNDAI', 
+    'Model' : 'IONIQ 5', 
+    'HDA' : 2 
+    } 
+# Dictionary : 이름을 붙여 저장 가능
+
+print(중고차2['Model'])
 
 # 점 찍는 함수들
 # 중고차.sort() / 정렬
 # 중고차.reverse() / 뒤집어 정렬
 # 중고차.pop() / 맨 뒤 자료를 추출
 
-print(중고차)
